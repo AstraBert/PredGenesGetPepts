882e350efbe50ce16870c07d1dfa1a766ca413f9
Astra Bertelli <133636879+AstraBert@users.noreply.github.com>
2023-12-10 11:58:56 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,38 @@
+echo "Installation process started, checking if Anaconda/Miniconda and Mamba are installed..."
+
+
+command -v conda >/dev/null 2>&1 || {
+   echo >&2 "The installation pipeline requires Anaconda/Miniconda but it is not installed. Please check here: https://anaconda.org/ for more details. Aborting."
+   exit 1
+}
+
+
+command -v mamba >/dev/null 2>&1 || {
+   echo >&2 "The installation pipeline requires Mamba but it is not installed. Please check here: https://github.com/conda-forge/miniforge#mambaforge for more details. Aborting."
+   exit 2
+}
+
+echo "Anaconda/Miniconda and Mamba are correctly installed, proceeding with the installation of necessary softwares: it may take a while..."
+
+sleep 1
+
+tmp=$(dirname $0)
+basedir=$(realpath $tmp)
+
+ConPath=$(which conda)
+tmp=${ConPath#* }
+Conda=${tmp%%/bin/co*}
+
+mkdir -p $basedir/environments
+
+mamba create \
+  -p $basedir/environments/predgenesgetpepts  \
+  -y \
+  -c conda-forge \
+  -c bioconda \
+  python=3.10
+
+
+cp -n $basedir/readaugustus.py $basedir/environments/predgenesgetpepts 
+cp -n $basedir/predgenesgetpepts.sh $basedir/environments/predgenesgetpepts 
+
